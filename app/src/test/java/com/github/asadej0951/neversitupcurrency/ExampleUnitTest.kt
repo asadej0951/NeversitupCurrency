c9dc459c9cd8c84ccc01c7ae04f8e4fe9275a095
asadej-wongnak <asadejwongnak@gmail.com>
2023-06-20 18:13:39 +0700

    [UPDATE]

--- app/src/test/java/com/github/asadej0951/neversitupcurrency/ExampleUnitTest.kt
+++ app/src/test/java/com/github/asadej0951/neversitupcurrency/ExampleUnitTest.kt
@@ -1,8 +1,11 @@
 package com.github.asadej0951.neversitupcurrency
 
+import android.content.Context
+import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +13,54 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun main() {
+        val n = 10
+        println("Fibonacci Sequence:")
+        for (i in 0 until n) {
+            val fibonacciNumber = fibonacci(i)
+            print("$fibonacciNumber ")
+        }
+    }
+
+    private fun fibonacci(n: Int): Int {
+        return if (n <= 1) {
+            n
+        } else {
+            fibonacci(n - 1) + fibonacci(n - 2)
+        }
+    }
+
+    @Test
+    fun primeNumber() {
+        val n = 10
+
+        println("\nPrime Numbers:")
+        var count = 0
+        var number = 2
+
+        while (count < n) {
+            if (isPrime(number)) {
+                print("$number ")
+                count++
+            }
+            number++
+        }
+    }
+
+    private fun isPrime(number: Int): Boolean {
+        if (number <= 1) {
+            return false
+        }
+
+        for (i in 2 until number) {
+            if (number % i == 0) {
+                return false
+            }
+        }
+
+        return true
     }
 }
